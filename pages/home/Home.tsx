c7d0ae56b1827911c8bd7dcf6e9157812abfbcbf
Silver <Silverg33k@gmail.com>
2022-10-05 22:41:18 +0100

    changed title

--- pages/home/Home.tsx
+++ pages/home/Home.tsx
@@ -7,7 +7,7 @@ import ThirdSection from "./ThirdSection";
 const Home = () => {
 	return (
 		<>
-        <title>Jiggylife</title>
+        <title>Jiggylife — Influencer lifestyle unscripted</title>
 			<FirstSection />
 			<SecondSection />
 			<ThirdSection />
